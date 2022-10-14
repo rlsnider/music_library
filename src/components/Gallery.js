9c90beb794f564459fbc9708ea2028eba748f891
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-10-14 14:54:06 -0700

    completed part 2

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,17 @@
 import GalleryItem from './GalleryItem';
 
 function Gallery (props) {
+    const display = props.data.map((item, index) => {
     return (
         <div>
-            <GalleryItem />
+            <GalleryItem item={item} key={index} />
         </div>
     )
+   })
+   return (
+    <div>
+        {display}
+    </div>
+   )
 }
 export default Gallery
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useState } from 'react'
+
     function SearchBar (props) {
         let [searchTerm, setSearchTerm] = useState('')
 
         return (
-            <form>
-                <input type="text" placeholder="Enter a search term here" />
+            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+
+                <input type="text" placeholder="Enter a search term here"
+                onChange={(e) => setSearchTerm(e.target.value)} />
+
                 <input type="submit" />
             </form>
         )
